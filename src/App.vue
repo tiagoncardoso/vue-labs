40fa5d59b9ae2e784d2a3cc37fec0d484d7bd1db
Tiago Cardoso <tiago.nascimento@dasa.com.br>
2022-02-03 07:38:22 -0300

    chore: Recreating Project Base

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <h1>Starting everything</h1>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {}
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
