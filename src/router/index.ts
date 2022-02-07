b4fc7515885cff12cebe2964dc63065a07ebd686
Tiago Cardoso <tiago.nascimento@dasa.com.br>
2022-02-07 08:51:40 -0300

    chore: Add Vuetify

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeVuetify from "../views/HomeVuetify.vue";
 
 Vue.use(VueRouter);
 
@@ -8,7 +8,7 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: HomeVuetify,
   },
   {
     path: "/about",
